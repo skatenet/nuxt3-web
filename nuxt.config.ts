7d513820cd7c732526c4d0f43dd3b4ae10c7ae3b
Stan Smith <choicedesign@gmail.com>
2022-09-18 18:05:18 -0600

    Add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
-  modules: ['@pinia/nuxt'],
+  plugins: [{ src: '~/plugins/oruga.ts' }],
+  head: {
+    // meta: [],
+    link: [
+      { rel: 'icon', href: '/favicon.ico', sizes: 'any' }, // 32×32
+      { rel: 'icon', href: '/icon.svg', type: 'image/svg+xml' },
+      { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }, // 180×180
+      { rel: 'manifest', href: '/manifest.webmanifest' },
+    ],
+    // style: [],
+    // script: [],
+    // noscript: [],
+    // charset: 'utf-8',
+    // viewport: 'width=device-width, initial-scale=1',
+  },
 })
